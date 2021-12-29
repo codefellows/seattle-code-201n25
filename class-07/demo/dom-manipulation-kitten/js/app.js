f712a9846175c562594091949e304c0c2735d674
sheyna <sheynawatkins@gmail.com>
2021-12-28 20:57:31 -0800

    added constructor functions to the kitten project.

--- class-07/demo/dom-manipulation-kitten/js/app.js
+++ class-07/demo/dom-manipulation-kitten/js/app.js
@@ -3,26 +3,83 @@
 const profileContainer = document.getElementById('kittenProfiles');
 const kittenTable = document.querySelector('table tbody');
 
-let jumper = {
-  name: 'Jumper',
-  about: 'Jumper the cat is super sweet!',
-  cutenessIndex: 10,
-  like: [
-    'cuddling',
-    'lazer pointer',
-    'cat nap'
-  ],
-  render: function () {
+let kittenArray = [];
+
+function Kitten(name, about, like, src) {
+  this.name = name;
+  this.about = about;
+  this.cutenessIndex = 10;
+  this.like = like;
+  this.src = src;
+  this.listId = `${this.name}Likes`;
+  this.render = function () {
+    // 1.) create element
+    let article = document.createElement('article');
+
+    // 2. or 3.) give it content
+    // 2. or 3.) append it to the DOM
+    profileContainer.appendChild(article);
+
+    // h3
+    let h3 = document.createElement('h3');
+    h3.textContent = this.name;
+    article.appendChild(h3);
+
+    // p
+    let p = document.createElement('p');
+    p.textContent = this.about;
+    article.appendChild(p);
+
+    // ul
+    let ul = document.createElement('ul');
+    ul.id = this.listId;
+    article.appendChild(ul);
+
+    // img
+    let img = document.createElement('img');
+    img.src = this.src;
+    img.alt = this.about;
+    article.appendChild(img);
+
+    this.renderList();
+  };
+  this.renderList = function () {
+    const kittenLikes = document.getElementById(this.listId);
     for (let i = 0; i < this.like.length; i++) {
       // li
       let li = document.createElement('li');
       li.textContent = `${this.name} likes ${this.like[i]}`;
-      ul.appendChild(li);
+      kittenLikes.appendChild(li);
     }
-  }
-};
+  };
+  kittenArray.push(this);
+}
 
-jumper.renderTableRow = function () {
+// Kitten(name, about, like, src)
+// let jumper = new Kitten(
+new Kitten(
+  'Jumper',
+  'Jumper the cat is super sweet!',
+  ['cuddling', 'lazer pointer', 'cat nap'],
+  'images/jumper.jpeg'
+);
+
+new Kitten(
+  'Serena',
+  'Serena is a sweet kitty',
+  ['string', 'feathers', 'soft beds'],
+  'images/serena.jpeg'
+);
+
+new Kitten(
+  'Frankie',
+  'Frankie is available for adoption',
+  ['naps', 'treats', 'cat tree'],
+  'images/frankie.jpeg'
+);
+//console.log(kittenArray);
+
+Kitten.prototype.renderTableRow = function () {
   let tr = document.createElement('tr');
   kittenTable.appendChild(tr);
   let tdName = document.createElement('td');
@@ -34,36 +91,69 @@ jumper.renderTableRow = function () {
     tr.appendChild(td);
   }
 };
-jumper.renderTableRow();
 
-// 1.) create element
-let article = document.createElement('article');
+// frankie.render();
+// jumper.render();
+// serena.render();
 
-// 2. or 3.) give it content
-// 2. or 3.) append it to the DOM
+// jumper.renderTableRow();
+// frankie.renderTableRow();
+// serena.renderTableRow();
 
+for (let i = 0; i < kittenArray.length; i++) {
+  kittenArray[i].render();
+  kittenArray[i].renderTableRow();
+}
 
-// h3
-let h3 = document.createElement('h3');
-h3.textContent = 'Jumper';
-article.appendChild(h3);
+// let jumper = {
+//   name: 'Jumper',
+//   about: 'Jumper the cat is super sweet!',
+//   cutenessIndex: 10,
+//   like: [
+//     'cuddling',
+//     'lazer pointer',
+//     'cat nap'
+//   ],
+//   src: 'images/jumper.jpeg',
+//   listId: `${this.name}Likes`,
+//   render: function () {
+//     // 1.) create element
+//     let article = document.createElement('article');
 
-// p
-let p = document.createElement('p');
-p.textContent = 'Jumper the cat is super sweet!';
-article.appendChild(p);
+//     // 2. or 3.) give it content
+//     // 2. or 3.) append it to the DOM
+//     profileContainer.appendChild(article);
 
-// ul
-let ul = document.createElement('ul');
-article.appendChild(ul);
+//     // h3
+//     let h3 = document.createElement('h3');
+//     h3.textContent = this.name;
+//     article.appendChild(h3);
 
-jumper.render();
+//     // p
+//     let p = document.createElement('p');
+//     p.textContent = this.about;
+//     article.appendChild(p);
 
-// img
-let img = document.createElement('img');
-img.src = 'images/jumper.jpeg';
-img.alt = 'Jumper the cat is super sweet!';
-article.appendChild(img);
+//     // ul
+//     let ul = document.createElement('ul');
+//     ul.id = this.listId;
+//     article.appendChild(ul);
 
-profileContainer.appendChild(article);
+//     // img
+//     let img = document.createElement('img');
+//     img.src = this.src;
+//     img.alt = this.about;
+//     article.appendChild(img);
 
+//     this.renderList();
+//   },
+//   renderList: function () {
+//     const kittenLikes = document.getElementById(this.listId);
+//     for (let i = 0; i < this.like.length; i++) {
+//       // li
+//       let li = document.createElement('li');
+//       li.textContent = `${this.name} likes ${this.like[i]}`;
+//       kittenLikes.appendChild(li);
+//     }
+//   },
+// };
